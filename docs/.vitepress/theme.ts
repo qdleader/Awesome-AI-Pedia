3001b6d5d3b8d8391d9875a776f50ba111ffe76d
yangkun <yangkun@icourt.cc>
2026-01-27 11:30:36 +0800

    feat: init project

--- docs/.vitepress/theme.ts
+++ docs/.vitepress/theme.ts
@@ -1,19 +1,5 @@
-import { h } from 'vue'
-import { useRoute } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import './custom.css'
-import BlogMeta from './components/BlogMeta.vue'
 
-export default DefaultTheme({
-  extends: DefaultTheme,
-  Layout() {
-    const route = useRoute()
-    return h(DefaultTheme.Layout, null, {
-      'doc-footer': () => h(BlogMeta)
-    })
-  },
-  enhanceApp({ app, router }) {
-    // 注册全局组件
-    app.component('BlogMeta', BlogMeta)
-  }
-})
+export default DefaultTheme
+
